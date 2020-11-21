16cd764223ec339f902cdcbdf8a98ced2b4a9aab
unknown <alishaafici@gmail.com>
2020-11-20 20:47:39 -0500

    added subscribe button and made incomplete function call when subscribe is call.

--- src/app/ngrx/actions/channel.actions.ts
+++ src/app/ngrx/actions/channel.actions.ts
@@ -1,4 +1,6 @@
 import { createAction, props } from '@ngrx/store';
+import { User } from 'twilio-chat';
 import { Channel } from '../../models/channel.model';
 
 export const startChat = createAction('[Channel] Start Chat', props<{ channel: Channel }>());
+export const subscribeChannel = createAction('[Channel] Subscribe', props<{channel: Channel, user: User}>());
