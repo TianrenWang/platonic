72528ad4a6377f6d75dc9b671c7f27ed524e23b3
Frank Wang <broodstar_frank@hotmail.com>
2021-05-08 18:25:11 -0400

    increased size limit on request body

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const path = require('path');
-const bodyParser = require('body-parser');
 const logger = require('morgan');
 const cors = require('cors');
 const passport = require('passport');
@@ -22,8 +21,8 @@ const app = express();
 // middleware
 app.use(cors());
 app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.json({limit: '25mb'}));
+app.use(express.urlencoded({limit: '25mb', extended: true}));
 app.use(passport.initialize());
 require('./config/passport').normal_authentication(passport);
 var distDir = __dirname + "/dist/";
