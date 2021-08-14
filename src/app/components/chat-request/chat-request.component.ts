97a0532177f5758c1f1c1669d5e6f7f4fcb60ebe
Frank Wang <broodstar_frank@hotmail.com>
2021-08-14 14:52:58 -0400

    user has to be a member to accept chat requests

--- src/app/components/chat-request/chat-request.component.ts
+++ src/app/components/chat-request/chat-request.component.ts
@@ -5,6 +5,7 @@ import { Observable, of } from 'rxjs';
 import { withLatestFrom } from 'rxjs/operators';
 import { ChatRequest } from 'src/app/models/chat_request.model';
 import { User } from 'src/app/models/user.model';
+import { Channels, selectIsMember } from 'src/app/ngrx/reducers/channels.reducer';
 import { selectUser, UserInfo } from 'src/app/ngrx/reducers/userinfo.reducer';
 import { AlertService } from 'src/app/services/alert/alert.service';
 
@@ -16,17 +17,26 @@ import { AlertService } from 'src/app/services/alert/alert.service';
 export class ChatRequestComponent {
   user$: Observable<User>;
   user: User;
+  isMember$: Observable<boolean>;
+  isMember: boolean;
 
   constructor(
     public dialogRef: MatDialogRef<ChatRequestComponent>,
     @Inject(MAT_DIALOG_DATA) public chatRequest: ChatRequest,
     private userinfoStore: Store<{userinfo: UserInfo}>,
+    private channelStore: Store<{channels: Channels}>,
     private alertService: AlertService) {
       this.user$ = this.userinfoStore.select(selectUser);
+      this.isMember$ = this.channelStore.select(selectIsMember);
       of(1).pipe(
         withLatestFrom(this.user$)).subscribe(
         ([_, user]) => {
           this.user = user;
+        });
+      of(1).pipe(
+        withLatestFrom(this.isMember$)).subscribe(
+        ([_, isMember]) => {
+          this.isMember = isMember;
         });
     }
 
@@ -36,11 +46,15 @@ export class ChatRequestComponent {
   
   acceptChatRequest(): void {
     if (!this.user){
-      this.alertService.alert("You need to login to request a chat.");
+      this.alertService.alert("You need to login to accept a request.");
       return;
     }
     if (this.user._id === this.chatRequest.user._id){
       this.alertService.alert("You cannot accept your own chat request.");
+      return;
+    }
+    if (this.isMember === false){
+      this.alertService.alert("You need to be a member to accept a request.");
       return;
     }
     this.dialogRef.close(true);
